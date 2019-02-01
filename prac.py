984a90144c2f1aa6352995835ba87d503255983d
codybaraks <earvinbaraka@gmail.com>
2019-02-01 18:08:58 +0300

    Design

---
+++ prac.py
@@ -0,0 +1,56 @@
+from math import sqrt, pow
+
+# maths
+x = 95.5  # assing x a value
+number_to_round = round(x)  # round x,
+
+# print(number_to_round)
+
+# round  num 45.6
+x = 45.6
+round_no = round(x)
+print(round_no)
+
+number = 144
+
+# to_sqrt = sqrt(number)
+# print(to_sqrt)
+
+# num = 34
+# power = pow(num, 2)
+# print(power)
+# 2^3
+
+
+# y = 64
+# the_sqrt = sqrt(y)
+# print(the_sqrt)
+#
+#
+#
+# num = 20
+# the_power = pow(num, 10)
+# print(the_power)
+
+text = "hellovwevwevwevwevwvwevwev"
+#
+# x = len(text)
+# print(x)
+
+
+upper = text.upper()
+print(text.upper())
+
+# print(upper)
+
+#
+# s = "brian ume nuna"
+# # f = len(s)
+# # print(f)
+# find = s.isupper()
+# print()
+
+name = input("enter name")
+age = int(input("enter age"))
+
+print(name,age)
